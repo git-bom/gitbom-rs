a9a76390ee8b1f3f5bacfaf98588d033a40e226b
Nell Shamrell <nells@microsoft.com>
2022-02-25 15:38:47 -0800

    wip
    Signed-off-by: Nell Shamrell <nells@microsoft.com>

--- src/lib.rs
+++ src/lib.rs
@@ -28,52 +28,40 @@ impl GitOid {
 
         hasher.digest().to_string()
     }
-}
 
-/*
-pub fn generate_sha1_git_oid(x: &[u8]) -> String {
-    let prefix = format!("blob {}\0", x.len());
-    let mut hasher = sha1_smol::Sha1::new();
+    #[cfg(feature = "std")]
+    pub fn generate_sha1_git_oid_from_buffer<R>(
+        mut reader: BufReader<R>,
+        expected_length: usize,
+    ) -> String
+    where
+        BufReader<R>: std::io::Read,
+    {
+        let prefix = format!("blob {}\0", expected_length);
+        let mut hasher = sha1_smol::Sha1::new();
 
-    hasher.update(prefix.as_bytes());
-    hasher.update(x);
+        hasher.update(prefix.as_bytes());
 
-    hasher.digest().to_string()
-}
-*/
-
-#[cfg(feature = "std")]
-pub fn generate_sha1_git_oid_from_buffer<R>(
-    mut reader: BufReader<R>,
-    expected_length: usize,
-) -> String
-where
-    BufReader<R>: std::io::Read,
-{
-    let prefix = format!("blob {}\0", expected_length);
-    let mut hasher = sha1_smol::Sha1::new();
-
-    hasher.update(prefix.as_bytes());
-
-    let mut buf = [0; 4096]; // linux default page size is 4096
-    let mut amount_read = 0;
-    loop {
-        let y = reader.read(&mut buf);
-        match y {
-            Ok(0) => {
+        let mut buf = [0; 4096]; // linux default page size is 4096
+        let mut amount_read = 0;
+        loop {
+            let y = reader.read(&mut buf);
+            match y {
+                Ok(0) => {
+                    break;
+                }
+                Ok(size) => {
+                    hasher.update(&buf[..size]);
+                    amount_read = amount_read + size;
+                }
+                Err(_) => {
                 break;
-            }
-            Ok(size) => {
-                hasher.update(&buf[..size]);
-                amount_read = amount_read + size;
-            }
-            Err(_) => {
-                break;
+                }
             }
         }
-    }
 
-    hasher.digest().to_string()
+        hasher.digest().to_string()
+    }
 }
 
 #[cfg(test)]
@@ -113,7 +101,11 @@ mod tests {
             Ok(f) => {
                 let reader = BufReader::new(f);
 
-                let hash = generate_sha1_git_oid_from_buffer(reader, 11);
+                let new_gitoid = GitOid {
+                    hash_algorithm: HashAlgorithm::SHA1
+                };
+
+                let result = new_gitoid.generate_sha1_git_oid_from_buffer(reader, 11);
 
                 assert_eq!("95d09f2b10159347eece71399a7e2e907ea3df4f", hash)
             }
